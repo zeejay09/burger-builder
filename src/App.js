07f99bf863f7113efb34dbf9baa95b57193dbe7b
zeejay09 <zeejaybelamide@gmail.com>
2020-04-06 13:00:05 +0800

    state with handler methods

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Jeff', age: 21},
+      { name: 'Jasmine', age: 21},
+      { name: 'Kyla', age: 21},
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App!</h1>
         <p>This is really working!</p>
-        <Person name="Jeff" age="21"/>
-        <Person name="Jasmine" age="21">I am awesome!</Person>
-        <Person name="Kyla" age="21"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am awesome!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
