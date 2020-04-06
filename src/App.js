a9add2e1394f3c73eb0e0d2be25451e566d17c5d
zeejay09 <zeejaybelamide@gmail.com>
2020-04-06 11:10:02 +0800

    changes the declaration of the app from function to a class.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I am a react App!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a react App!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
