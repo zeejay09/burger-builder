720791edd941bc731a5d643bbba630de16d49be5
zeejay09 <zeejaybelamide@gmail.com>
2020-04-06 14:39:53 +0800

    reverts to class-based component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const App = props => {
+class App extends Component {
 
-  const [ personsState, setPersonsState ] = useState({
-      persons: [
-        { name: 'Jeff', age: 21},
-        { name: 'Jasmine', age: 21},
-        { name: 'Kyla', age: 21},
-      ]
-  });
+  state = {
+    persons: [
+      { name: 'Jeff', age: 21},
+      { name: 'Jasmine', age: 21},
+      { name: 'Kyla', age: 21},
+    ]
+  }
 
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // console.log('Was clicked!');
-    setPersonsState({
+    this.setState({
       persons: [
         { name: 'Zeejay', age: 21},
         { name: 'Jazzmine', age: 21},
@@ -23,16 +23,18 @@ const App = props => {
     });
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I am a React App!</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>I am awesome!</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App!</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am awesome!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
