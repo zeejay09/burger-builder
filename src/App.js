7523c4dfcf1a635ecc8cb53353e6974e61d66a77
zeejay09 <zeejaybelamide@gmail.com>
2020-04-27 15:27:33 +0800

    adds namechangehandler in the component which output a list of person

--- src/App.js
+++ src/App.js
@@ -13,13 +13,22 @@ class App extends Component {
     showPersons: false,
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Jeff', age: 21},
-        { name: event.target.value, age: 21},
-        { name: 'Kyla', age: 21},
-      ] 
+      persons: persons
     });
   }
 
@@ -52,7 +61,7 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person key={person.id} click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} />
+            return <Person key={person.id} click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
